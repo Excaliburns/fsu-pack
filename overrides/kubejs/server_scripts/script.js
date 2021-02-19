1b00a800b14eda9af32c3aec29bcc25707ba0483
Excaliburns <tutsandallthat@gmail.com>
2021-02-19 10:25:39 -0500

    change scripts

--- overrides/kubejs/server_scripts/script.js
+++ overrides/kubejs/server_scripts/script.js
@@ -8,7 +8,21 @@ settings.logErroringRecipes = true
 console.info('Hello, World! (You will see this line every time server resources reload)')
 
 onEvent('recipes', event => {
-  // Change recipes here
+  event.remove({id: 'buildinggadgets:gadget_exchanging'}) 
+  
+  // Mekanism
+  event.recipes.mekanism.enriching(item.of('immersiveengineering:dust_aluminum', 2), '#forge:ores/aluminum')
+  event.recipes.mekanism.crushing(item.of('immersiveengineering:dust_aluminum'), '#forge:ingots/aluminum')
+  
+  event.recipes.mekanism.enriching(item.of('thermal:silver_dust', 2), '#forge:ores/silver')
+  event.recipes.mekanism.crushing(item.of('thermal:silver_dust'), '#forge:ingots/silver')
+  
+  event.recipes.mekanism.enriching(item.of('thermal:nickel_dust', 2), '#forge:ores/nickel')
+  event.recipes.mekanism.crushing(item.of('thermal:nickel_dust'), '#forge:ingots/nickel')
+  
+  event.recipes.mekanism.enriching(item.of('create:crushed_zinc_ore', 2), '#forge:ores/zinc')
+  
+  event.recipes.mekanism.crushing(item.of('mana-and-artifice:vinteum_dust'), 'mana-and-artifice:vinteum_ingot')
 })
 
 onEvent('item.tags', event => {
