60d952446a77a557e65efd9cd4f81e6abf5630fa
Excaliburns <tutsandallthat@gmail.com>
2021-02-20 16:45:08 -0500

    change op recipes

--- overrides/kubejs/server_scripts/script.js
+++ overrides/kubejs/server_scripts/script.js
@@ -8,10 +8,41 @@ settings.logErroringRecipes = true
 console.info('Hello, World! (You will see this line every time server resources reload)')
 
 onEvent('recipes', event => {
+  // Remove recipes
+  // Exchanging Gadget
+  event.remove({id: 'buildinggadgets:gadget_exchanging'}) 
+
+  // Angel ring
+  event.remove({id: 'angelring:itemring'})
+
+  // Environmental Tech
+  event.remove({output: '/envirocore:litherite_crystal/'})
+
+  // Add recipes
+  // Angel ring
+  event.shaped(item.of('angelring:itemring', 1), [
+    'ABA',
+    'CEC',
+    'ADA'
+  ],{
+    A: 'botania:gaia_ingot',
+    B: 'astralsorcery:mantle',
+    C: 'bloodmagic:soulgemgreater',
+    D: 'appliedenergistics2:singularity',
+    E: 'angelring:itemdiamondring'
+  })
+
+  // Environmental Tech
+  event.shapeless('1x envirocore:litherite_crystal', [
+    'minecraft:netherite_ingot', 
+    'bloodmagic:demonslate',
+    'mekanism:alloy_atomic',
+    'pneumaticcraft:transistor'
+  ])
+
+  // Mana and artifice
   event.shapeless('1x mana-and-artifice:guide_book', ['minecraft:book', 'mana-and-artifice:vinteum_ingot'])
 
-  event.remove({id: 'buildinggadgets:gadget_exchanging'}) 
-  
   // Mekanism
   event.recipes.mekanism.enriching(item.of('immersiveengineering:dust_aluminum', 2), '#forge:ores/aluminum')
   event.recipes.mekanism.crushing(item.of('immersiveengineering:dust_aluminum'), '#forge:ingots/aluminum')
@@ -28,6 +59,7 @@ onEvent('recipes', event => {
 })
 
 onEvent('item.entity_interact',  event => {
+  // Remove donkey duping
   if(event.server 
     && event.item.id.equals("industrialforegoing:mob_imprisonment_tool") 
     && event.target.type.equals('minecraft:donkey')) {
